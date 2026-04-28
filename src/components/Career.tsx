c04b90a25a5428467415aa16a6c70cccaea1fd3a
Rajesh Kumar Pattanaik <rajeshpattanaik.apiman@gmail.com>
2026-04-28 07:46:10 +0530

    Third Commit

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -27,7 +27,7 @@ const Career = () => {
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Junior Developer</h4>
+                <h4>Application Developer</h4>
                 <h5>BoldTribe Innovation Private Limited</h5>
               </div>
               <h3>2025</h3>
